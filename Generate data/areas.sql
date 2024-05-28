186122da39cc8ef36b0fb969723a2562cf5af160
Bina Cohen <46712931+BinaCohen@users.noreply.github.com>
2024-05-28 23:49:03 +0300

    Add files via upload

---
+++ Generate data/areas.sql
@@ -0,0 +1,800 @@
+INSERT INTO AREAS (area_name)
+VALUES ('Nevada');
+INSERT INTO AREAS (area_name)
+VALUES ('FL');
+INSERT INTO AREAS (area_name)
+VALUES ('Kentucky');
+INSERT INTO AREAS (area_name)
+VALUES ('Washington');
+INSERT INTO AREAS (area_name)
+VALUES ('Minnesota');
+INSERT INTO AREAS (area_name)
+VALUES ('FL');
+INSERT INTO AREAS (area_name)
+VALUES ('VT');
+INSERT INTO AREAS (area_name)
+VALUES ('AR');
+INSERT INTO AREAS (area_name)
+VALUES ('DE');
+INSERT INTO AREAS (area_name)
+VALUES ('PA');
+INSERT INTO AREAS (area_name)
+VALUES ('Florida');
+INSERT INTO AREAS (area_name)
+VALUES ('Wyoming');
+INSERT INTO AREAS (area_name)
+VALUES ('IN');
+INSERT INTO AREAS (area_name)
+VALUES ('Hawaii');
+INSERT INTO AREAS (area_name)
+VALUES ('ME');
+INSERT INTO AREAS (area_name)
+VALUES ('Oregon');
+INSERT INTO AREAS (area_name)
+VALUES ('Kansas');
+INSERT INTO AREAS (area_name)
+VALUES ('AK');
+INSERT INTO AREAS (area_name)
+VALUES ('PA');
+INSERT INTO AREAS (area_name)
+VALUES ('LA');
+INSERT INTO AREAS (area_name)
+VALUES ('OK');
+INSERT INTO AREAS (area_name)
+VALUES ('Oregon');
+INSERT INTO AREAS (area_name)
+VALUES ('AL');
+INSERT INTO AREAS (area_name)
+VALUES ('Kansas');
+INSERT INTO AREAS (area_name)
+VALUES ('WY');
+INSERT INTO AREAS (area_name)
+VALUES ('GA');
+INSERT INTO AREAS (area_name)
+VALUES ('LA');
+INSERT INTO AREAS (area_name)
+VALUES ('Minnesota');
+INSERT INTO AREAS (area_name)
+VALUES ('Tennessee');
+INSERT INTO AREAS (area_name)
+VALUES ('California');
+INSERT INTO AREAS (area_name)
+VALUES ('Nevada');
+INSERT INTO AREAS (area_name)
+VALUES ('Hawaii');
+INSERT INTO AREAS (area_name)
+VALUES ('Utah');
+INSERT INTO AREAS (area_name)
+VALUES ('Tennessee');
+INSERT INTO AREAS (area_name)
+VALUES ('IN');
+INSERT INTO AREAS (area_name)
+VALUES ('California');
+INSERT INTO AREAS (area_name)
+VALUES ('LA');
+INSERT INTO AREAS (area_name)
+VALUES ('NV');
+INSERT INTO AREAS (area_name)
+VALUES ('Massachusetts');
+INSERT INTO AREAS (area_name)
+VALUES ('Arkansas');
+INSERT INTO AREAS (area_name)
+VALUES ('NE');
+INSERT INTO AREAS (area_name)
+VALUES ('Vermont');
+INSERT INTO AREAS (area_name)
+VALUES ('Virginia');
+INSERT INTO AREAS (area_name)
+VALUES ('OR');
+INSERT INTO AREAS (area_name)
+VALUES ('ME');
+INSERT INTO AREAS (area_name)
+VALUES ('KY');
+INSERT INTO AREAS (area_name)
+VALUES ('Nebraska');
+INSERT INTO AREAS (area_name)
+VALUES ('Alaska');
+INSERT INTO AREAS (area_name)
+VALUES ('LA');
+INSERT INTO AREAS (area_name)
+VALUES ('WY');
+INSERT INTO AREAS (area_name)
+VALUES ('Delaware');
+INSERT INTO AREAS (area_name)
+VALUES ('Oklahoma');
+INSERT INTO AREAS (area_name)
+VALUES ('PA');
+INSERT INTO AREAS (area_name)
+VALUES ('ID');
+INSERT INTO AREAS (area_name)
+VALUES ('Vermont');
+INSERT INTO AREAS (area_name)
+VALUES ('Hawaii');
+INSERT INTO AREAS (area_name)
+VALUES ('VA');
+INSERT INTO AREAS (area_name)
+VALUES ('Florida');
+INSERT INTO AREAS (area_name)
+VALUES ('CO');
+INSERT INTO AREAS (area_name)
+VALUES ('Kansas');
+INSERT INTO AREAS (area_name)
+VALUES ('IN');
+INSERT INTO AREAS (area_name)
+VALUES ('IL');
+INSERT INTO AREAS (area_name)
+VALUES ('IA');
+INSERT INTO AREAS (area_name)
+VALUES ('AL');
+INSERT INTO AREAS (area_name)
+VALUES ('California');
+INSERT INTO AREAS (area_name)
+VALUES ('Maryland');
+INSERT INTO AREAS (area_name)
+VALUES ('VA');
+INSERT INTO AREAS (area_name)
+VALUES ('Kentucky');
+INSERT INTO AREAS (area_name)
+VALUES ('UT');
+INSERT INTO AREAS (area_name)
+VALUES ('Utah');
+INSERT INTO AREAS (area_name)
+VALUES ('ME');
+INSERT INTO AREAS (area_name)
+VALUES ('Nebraska');
+INSERT INTO AREAS (area_name)
+VALUES ('Ohio');
+INSERT INTO AREAS (area_name)
+VALUES ('Montana');
+INSERT INTO AREAS (area_name)
+VALUES ('WI');
+INSERT INTO AREAS (area_name)
+VALUES ('LA');
+INSERT INTO AREAS (area_name)
+VALUES ('NV');
+INSERT INTO AREAS (area_name)
+VALUES ('Indiana');
+INSERT INTO AREAS (area_name)
+VALUES ('WA');
+INSERT INTO AREAS (area_name)
+VALUES ('Michigan');
+INSERT INTO AREAS (area_name)
+VALUES ('Mississippi');
+INSERT INTO AREAS (area_name)
+VALUES ('MT');
+INSERT INTO AREAS (area_name)
+VALUES ('Connecticut');
+INSERT INTO AREAS (area_name)
+VALUES ('Pennsylvania');
+INSERT INTO AREAS (area_name)
+VALUES ('HI');
+INSERT INTO AREAS (area_name)
+VALUES ('OK');
+INSERT INTO AREAS (area_name)
+VALUES ('Louisiana');
+INSERT INTO AREAS (area_name)
+VALUES ('Louisiana');
+INSERT INTO AREAS (area_name)
+VALUES ('PA');
+INSERT INTO AREAS (area_name)
+VALUES ('Louisiana');
+INSERT INTO AREAS (area_name)
+VALUES ('California');
+INSERT INTO AREAS (area_name)
+VALUES ('Connecticut');
+INSERT INTO AREAS (area_name)
+VALUES ('Wisconsin');
+INSERT INTO AREAS (area_name)
+VALUES ('IA');
+INSERT INTO AREAS (area_name)
+VALUES ('Oregon');
+INSERT INTO AREAS (area_name)
+VALUES ('CA');
+INSERT INTO AREAS (area_name)
+VALUES ('MN');
+INSERT INTO AREAS (area_name)
+VALUES ('Mississippi');
+INSERT INTO AREAS (area_name)
+VALUES ('Washington');
+INSERT INTO AREAS (area_name)
+VALUES ('Utah');
+INSERT INTO AREAS (area_name)
+VALUES ('WI');
+INSERT INTO AREAS (area_name)
+VALUES ('DE');
+INSERT INTO AREAS (area_name)
+VALUES ('WI');
+INSERT INTO AREAS (area_name)
+VALUES ('AZ');
+INSERT INTO AREAS (area_name)
+VALUES ('NE');
+INSERT INTO AREAS (area_name)
+VALUES ('Connecticut');
+INSERT INTO AREAS (area_name)
+VALUES ('IA');
+INSERT INTO AREAS (area_name)
+VALUES ('Delaware');
+INSERT INTO AREAS (area_name)
+VALUES ('UT');
+INSERT INTO AREAS (area_name)
+VALUES ('Indiana');
+INSERT INTO AREAS (area_name)
+VALUES ('Colorado');
+INSERT INTO AREAS (area_name)
+VALUES ('Oregon');
+INSERT INTO AREAS (area_name)
+VALUES ('Idaho');
+INSERT INTO AREAS (area_name)
+VALUES ('Montana');
+INSERT INTO AREAS (area_name)
+VALUES ('NV');
+INSERT INTO AREAS (area_name)
+VALUES ('IN');
+INSERT INTO AREAS (area_name)
+VALUES ('Michigan');
+INSERT INTO AREAS (area_name)
+VALUES ('Georgia');
+INSERT INTO AREAS (area_name)
+VALUES ('Mississippi');
+INSERT INTO AREAS (area_name)
+VALUES ('Arizona');
+INSERT INTO AREAS (area_name)
+VALUES ('WY');
+INSERT INTO AREAS (area_name)
+VALUES ('Tennessee');
+INSERT INTO AREAS (area_name)
+VALUES ('Arkansas');
+INSERT INTO AREAS (area_name)
+VALUES ('PA');
+INSERT INTO AREAS (area_name)
+VALUES ('MN');
+INSERT INTO AREAS (area_name)
+VALUES ('Arkansas');
+INSERT INTO AREAS (area_name)
+VALUES ('Virginia');
+INSERT INTO AREAS (area_name)
+VALUES ('CA');
+INSERT INTO AREAS (area_name)
+VALUES ('UT');
+INSERT INTO AREAS (area_name)
+VALUES ('AL');
+INSERT INTO AREAS (area_name)
+VALUES ('DE');
+INSERT INTO AREAS (area_name)
+VALUES ('Wyoming');
+INSERT INTO AREAS (area_name)
+VALUES ('Arkansas');
+INSERT INTO AREAS (area_name)
+VALUES ('TN');
+INSERT INTO AREAS (area_name)
+VALUES ('Michigan');
+INSERT INTO AREAS (area_name)
+VALUES ('Wisconsin');
+INSERT INTO AREAS (area_name)
+VALUES ('Georgia');
+INSERT INTO AREAS (area_name)
+VALUES ('Nebraska');
+INSERT INTO AREAS (area_name)
+VALUES ('HI');
+INSERT INTO AREAS (area_name)
+VALUES ('UT');
+INSERT INTO AREAS (area_name)
+VALUES ('MD');
+INSERT INTO AREAS (area_name)
+VALUES ('Georgia');
+INSERT INTO AREAS (area_name)
+VALUES ('Arkansas');
+INSERT INTO AREAS (area_name)
+VALUES ('Virginia');
+INSERT INTO AREAS (area_name)
+VALUES ('IA');
+INSERT INTO AREAS (area_name)
+VALUES ('NE');
+INSERT INTO AREAS (area_name)
+VALUES ('AK');
+INSERT INTO AREAS (area_name)
+VALUES ('AR');
+INSERT INTO AREAS (area_name)
+VALUES ('CT');
+INSERT INTO AREAS (area_name)
+VALUES ('Wyoming');
+INSERT INTO AREAS (area_name)
+VALUES ('California');
+INSERT INTO AREAS (area_name)
+VALUES ('TN');
+INSERT INTO AREAS (area_name)
+VALUES ('AR');
+INSERT INTO AREAS (area_name)
+VALUES ('Utah');
+INSERT INTO AREAS (area_name)
+VALUES ('Hawaii');
+INSERT INTO AREAS (area_name)
+VALUES ('OR');
+INSERT INTO AREAS (area_name)
+VALUES ('IN');
+INSERT INTO AREAS (area_name)
+VALUES ('Pennsylvania');
+INSERT INTO AREAS (area_name)
+VALUES ('MT');
+INSERT INTO AREAS (area_name)
+VALUES ('HI');
+INSERT INTO AREAS (area_name)
+VALUES ('NV');
+INSERT INTO AREAS (area_name)
+VALUES ('Wyoming');
+INSERT INTO AREAS (area_name)
+VALUES ('CA');
+INSERT INTO AREAS (area_name)
+VALUES ('Louisiana');
+INSERT INTO AREAS (area_name)
+VALUES ('Delaware');
+INSERT INTO AREAS (area_name)
+VALUES ('Connecticut');
+INSERT INTO AREAS (area_name)
+VALUES ('Pennsylvania');
+INSERT INTO AREAS (area_name)
+VALUES ('FL');
+INSERT INTO AREAS (area_name)
+VALUES ('Kansas');
+INSERT INTO AREAS (area_name)
+VALUES ('VT');
+INSERT INTO AREAS (area_name)
+VALUES ('Maryland');
+INSERT INTO AREAS (area_name)
+VALUES ('MD');
+INSERT INTO AREAS (area_name)
+VALUES ('Florida');
+INSERT INTO AREAS (area_name)
+VALUES ('MT');
+INSERT INTO AREAS (area_name)
+VALUES ('FL');
+INSERT INTO AREAS (area_name)
+VALUES ('IL');
+INSERT INTO AREAS (area_name)
+VALUES ('Alabama');
+INSERT INTO AREAS (area_name)
+VALUES ('LA');
+INSERT INTO AREAS (area_name)
+VALUES ('Massachusetts');
+INSERT INTO AREAS (area_name)
+VALUES ('Virginia');
+INSERT INTO AREAS (area_name)
+VALUES ('OH');
+INSERT INTO AREAS (area_name)
+VALUES ('Wisconsin');
+INSERT INTO AREAS (area_name)
+VALUES ('Vermont');
+INSERT INTO AREAS (area_name)
+VALUES ('Arkansas');
+INSERT INTO AREAS (area_name)
+VALUES ('Kansas');
+INSERT INTO AREAS (area_name)
+VALUES ('Pennsylvania');
+INSERT INTO AREAS (area_name)
+VALUES ('AK');
+INSERT INTO AREAS (area_name)
+VALUES ('Maryland');
+INSERT INTO AREAS (area_name)
+VALUES ('Florida');
+INSERT INTO AREAS (area_name)
+VALUES ('Michigan');
+INSERT INTO AREAS (area_name)
+VALUES ('Alaska');
+INSERT INTO AREAS (area_name)
+VALUES ('FL');
+INSERT INTO AREAS (area_name)
+VALUES ('ME');
+INSERT INTO AREAS (area_name)
+VALUES ('ME');
+INSERT INTO AREAS (area_name)
+VALUES ('KY');
+INSERT INTO AREAS (area_name)
+VALUES ('DE');
+INSERT INTO AREAS (area_name)
+VALUES ('WY');
+INSERT INTO AREAS (area_name)
+VALUES ('Kansas');
+INSERT INTO AREAS (area_name)
+VALUES ('Texas');
+INSERT INTO AREAS (area_name)
+VALUES ('MT');
+INSERT INTO AREAS (area_name)
+VALUES ('Florida');
+INSERT INTO AREAS (area_name)
+VALUES ('Minnesota');
+INSERT INTO AREAS (area_name)
+VALUES ('Georgia');
+INSERT INTO AREAS (area_name)
+VALUES ('Hawaii');
+INSERT INTO AREAS (area_name)
+VALUES ('Oklahoma');
+INSERT INTO AREAS (area_name)
+VALUES ('DE');
+INSERT INTO AREAS (area_name)
+VALUES ('WA');
+INSERT INTO AREAS (area_name)
+VALUES ('PA');
+INSERT INTO AREAS (area_name)
+VALUES ('AZ');
+INSERT INTO AREAS (area_name)
+VALUES ('Virginia');
+INSERT INTO AREAS (area_name)
+VALUES ('WY');
+INSERT INTO AREAS (area_name)
+VALUES ('Colorado');
+INSERT INTO AREAS (area_name)
+VALUES ('Montana');
+INSERT INTO AREAS (area_name)
+VALUES ('OR');
+INSERT INTO AREAS (area_name)
+VALUES ('Mississippi');
+INSERT INTO AREAS (area_name)
+VALUES ('Texas');
+INSERT INTO AREAS (area_name)
+VALUES ('Georgia');
+INSERT INTO AREAS (area_name)
+VALUES ('KS');
+INSERT INTO AREAS (area_name)
+VALUES ('Georgia');
+INSERT INTO AREAS (area_name)
+VALUES ('Washington');
+INSERT INTO AREAS (area_name)
+VALUES ('HI');
+INSERT INTO AREAS (area_name)
+VALUES ('Nebraska');
+INSERT INTO AREAS (area_name)
+VALUES ('MT');
+INSERT INTO AREAS (area_name)
+VALUES ('VA');
+INSERT INTO AREAS (area_name)
+VALUES ('Vermont');
+INSERT INTO AREAS (area_name)
+VALUES ('MT');
+INSERT INTO AREAS (area_name)
+VALUES ('Massachusetts');
+INSERT INTO AREAS (area_name)
+VALUES ('Nevada');
+INSERT INTO AREAS (area_name)
+VALUES ('Kentucky');
+INSERT INTO AREAS (area_name)
+VALUES ('VT');
+INSERT INTO AREAS (area_name)
+VALUES ('Utah');
+INSERT INTO AREAS (area_name)
+VALUES ('Kentucky');
+INSERT INTO AREAS (area_name)
+VALUES ('VT');
+INSERT INTO AREAS (area_name)
+VALUES ('AZ');
+INSERT INTO AREAS (area_name)
+VALUES ('LA');
+INSERT INTO AREAS (area_name)
+VALUES ('Washington');
+INSERT INTO AREAS (area_name)
+VALUES ('FL');
+INSERT INTO AREAS (area_name)
+VALUES ('Wyoming');
+INSERT INTO AREAS (area_name)
+VALUES ('LA');
+INSERT INTO AREAS (area_name)
+VALUES ('NE');
+INSERT INTO AREAS (area_name)
+VALUES ('Mississippi');
+INSERT INTO AREAS (area_name)
+VALUES ('California');
+INSERT INTO AREAS (area_name)
+VALUES ('Utah');
+INSERT INTO AREAS (area_name)
+VALUES ('Tennessee');
+INSERT INTO AREAS (area_name)
+VALUES ('Kansas');
+INSERT INTO AREAS (area_name)
+VALUES ('Wisconsin');
+INSERT INTO AREAS (area_name)
+VALUES ('MN');
+INSERT INTO AREAS (area_name)
+VALUES ('MN');
+INSERT INTO AREAS (area_name)
+VALUES ('Louisiana');
+INSERT INTO AREAS (area_name)
+VALUES ('OK');
+INSERT INTO AREAS (area_name)
+VALUES ('MN');
+INSERT INTO AREAS (area_name)
+VALUES ('LA');
+INSERT INTO AREAS (area_name)
+VALUES ('ID');
+INSERT INTO AREAS (area_name)
+VALUES ('GA');
+INSERT INTO AREAS (area_name)
+VALUES ('Maine');
+INSERT INTO AREAS (area_name)
+VALUES ('AL');
+INSERT INTO AREAS (area_name)
+VALUES ('Colorado');
+INSERT INTO AREAS (area_name)
+VALUES ('Illinois');
+INSERT INTO AREAS (area_name)
+VALUES ('DE');
+INSERT INTO AREAS (area_name)
+VALUES ('Louisiana');
+INSERT INTO AREAS (area_name)
+VALUES ('TN');
+INSERT INTO AREAS (area_name)
+VALUES ('Washington');
+INSERT INTO AREAS (area_name)
+VALUES ('ME');
+INSERT INTO AREAS (area_name)
+VALUES ('AR');
+INSERT INTO AREAS (area_name)
+VALUES ('Arkansas');
+INSERT INTO AREAS (area_name)
+VALUES ('MD');
+INSERT INTO AREAS (area_name)
+VALUES ('Mississippi');
+INSERT INTO AREAS (area_name)
+VALUES ('Oregon');
+INSERT INTO AREAS (area_name)
+VALUES ('Alabama');
+INSERT INTO AREAS (area_name)
+VALUES ('Arizona');
+INSERT INTO AREAS (area_name)
+VALUES ('UT');
+INSERT INTO AREAS (area_name)
+VALUES ('MS');
+INSERT INTO AREAS (area_name)
+VALUES ('ME');
+INSERT INTO AREAS (area_name)
+VALUES ('MA');
+INSERT INTO AREAS (area_name)
+VALUES ('Maine');
+INSERT INTO AREAS (area_name)
+VALUES ('Idaho');
+INSERT INTO AREAS (area_name)
+VALUES ('DE');
+INSERT INTO AREAS (area_name)
+VALUES ('VT');
+INSERT INTO AREAS (area_name)
+VALUES ('Mississippi');
+INSERT INTO AREAS (area_name)
+VALUES ('WA');
+INSERT INTO AREAS (area_name)
+VALUES ('Maine');
+INSERT INTO AREAS (area_name)
+VALUES ('IN');
+INSERT INTO AREAS (area_name)
+VALUES ('Tennessee');
+INSERT INTO AREAS (area_name)
+VALUES ('MT');
+INSERT INTO AREAS (area_name)
+VALUES ('Illinois');
+INSERT INTO AREAS (area_name)
+VALUES ('NV');
+INSERT INTO AREAS (area_name)
+VALUES ('FL');
+INSERT INTO AREAS (area_name)
+VALUES ('AR');
+INSERT INTO AREAS (area_name)
+VALUES ('Texas');
+INSERT INTO AREAS (area_name)
+VALUES ('Minnesota');
+INSERT INTO AREAS (area_name)
+VALUES ('Maine');
+INSERT INTO AREAS (area_name)
+VALUES ('Kansas');
+INSERT INTO AREAS (area_name)
+VALUES ('Maryland');
+INSERT INTO AREAS (area_name)
+VALUES ('AR');
+INSERT INTO AREAS (area_name)
+VALUES ('MT');
+INSERT INTO AREAS (area_name)
+VALUES ('AK');
+INSERT INTO AREAS (area_name)
+VALUES ('MD');
+INSERT INTO AREAS (area_name)
+VALUES ('PA');
+INSERT INTO AREAS (area_name)
+VALUES ('CA');
+INSERT INTO AREAS (area_name)
+VALUES ('Colorado');
+INSERT INTO AREAS (area_name)
+VALUES ('Kentucky');
+INSERT INTO AREAS (area_name)
+VALUES ('Louisiana');
+INSERT INTO AREAS (area_name)
+VALUES ('AK');
+INSERT INTO AREAS (area_name)
+VALUES ('Arizona');
+INSERT INTO AREAS (area_name)
+VALUES ('Illinois');
+INSERT INTO AREAS (area_name)
+VALUES ('KS');
+INSERT INTO AREAS (area_name)
+VALUES ('MA');
+INSERT INTO AREAS (area_name)
+VALUES ('MI');
+INSERT INTO AREAS (area_name)
+VALUES ('Wyoming');
+INSERT INTO AREAS (area_name)
+VALUES ('LA');
+INSERT INTO AREAS (area_name)
+VALUES ('CO');
+INSERT INTO AREAS (area_name)
+VALUES ('KS');
+INSERT INTO AREAS (area_name)
+VALUES ('MN');
+INSERT INTO AREAS (area_name)
+VALUES ('Ohio');
+INSERT INTO AREAS (area_name)
+VALUES ('Tennessee');
+INSERT INTO AREAS (area_name)
+VALUES ('Oklahoma');
+INSERT INTO AREAS (area_name)
+VALUES ('California');
+INSERT INTO AREAS (area_name)
+VALUES ('NE');
+INSERT INTO AREAS (area_name)
+VALUES ('MO');
+INSERT INTO AREAS (area_name)
+VALUES ('OH');
+INSERT INTO AREAS (area_name)
+VALUES ('Arizona');
+INSERT INTO AREAS (area_name)
+VALUES ('Nebraska');
+INSERT INTO AREAS (area_name)
+VALUES ('ME');
+INSERT INTO AREAS (area_name)
+VALUES ('AZ');
+INSERT INTO AREAS (area_name)
+VALUES ('MI');
+INSERT INTO AREAS (area_name)
+VALUES ('Hawaii');
+INSERT INTO AREAS (area_name)
+VALUES ('FL');
+INSERT INTO AREAS (area_name)
+VALUES ('OK');
+INSERT INTO AREAS (area_name)
+VALUES ('Georgia');
+INSERT INTO AREAS (area_name)
+VALUES ('MO');
+INSERT INTO AREAS (area_name)
+VALUES ('CO');
+INSERT INTO AREAS (area_name)
+VALUES ('Colorado');
+INSERT INTO AREAS (area_name)
+VALUES ('TN');
+INSERT INTO AREAS (area_name)
+VALUES ('Virginia');
+INSERT INTO AREAS (area_name)
+VALUES ('ME');
+INSERT INTO AREAS (area_name)
+VALUES ('OR');
+INSERT INTO AREAS (area_name)
+VALUES ('VA');
+INSERT INTO AREAS (area_name)
+VALUES ('KY');
+INSERT INTO AREAS (area_name)
+VALUES ('ID');
+INSERT INTO AREAS (area_name)
+VALUES ('Pennsylvania');
+INSERT INTO AREAS (area_name)
+VALUES ('MT');
+INSERT INTO AREAS (area_name)
+VALUES ('MI');
+INSERT INTO AREAS (area_name)
+VALUES ('CT');
+INSERT INTO AREAS (area_name)
+VALUES ('TX');
+INSERT INTO AREAS (area_name)
+VALUES ('Illinois');
+INSERT INTO AREAS (area_name)
+VALUES ('KS');
+INSERT INTO AREAS (area_name)
+VALUES ('Maryland');
+INSERT INTO AREAS (area_name)
+VALUES ('OH');
+INSERT INTO AREAS (area_name)
+VALUES ('Alaska');
+INSERT INTO AREAS (area_name)
+VALUES ('Alaska');
+INSERT INTO AREAS (area_name)
+VALUES ('Indiana');
+INSERT INTO AREAS (area_name)
+VALUES ('California');
+INSERT INTO AREAS (area_name)
+VALUES ('Indiana');
+INSERT INTO AREAS (area_name)
+VALUES ('Pennsylvania');
+INSERT INTO AREAS (area_name)
+VALUES ('MA');
+INSERT INTO AREAS (area_name)
+VALUES ('MO');
+INSERT INTO AREAS (area_name)
+VALUES ('OR');
+INSERT INTO AREAS (area_name)
+VALUES ('TN');
+INSERT INTO AREAS (area_name)
+VALUES ('MS');
+INSERT INTO AREAS (area_name)
+VALUES ('MN');
+INSERT INTO AREAS (area_name)
+VALUES ('KY');
+INSERT INTO AREAS (area_name)
+VALUES ('KS');
+INSERT INTO AREAS (area_name)
+VALUES ('Oregon');
+INSERT INTO AREAS (area_name)
+VALUES ('Massachusetts');
+INSERT INTO AREAS (area_name)
+VALUES ('MI');
+INSERT INTO AREAS (area_name)
+VALUES ('Illinois');
+INSERT INTO AREAS (area_name)
+VALUES ('Nevada');
+INSERT INTO AREAS (area_name)
+VALUES ('Virginia');
+INSERT INTO AREAS (area_name)
+VALUES ('IL');
+INSERT INTO AREAS (area_name)
+VALUES ('Virginia');
+INSERT INTO AREAS (area_name)
+VALUES ('Utah');
+INSERT INTO AREAS (area_name)
+VALUES ('MA');
+INSERT INTO AREAS (area_name)
+VALUES ('Washington');
+INSERT INTO AREAS (area_name)
+VALUES ('MT');
+INSERT INTO AREAS (area_name)
+VALUES ('PA');
+INSERT INTO AREAS (area_name)
+VALUES ('Oregon');
+INSERT INTO AREAS (area_name)
+VALUES ('AK');
+INSERT INTO AREAS (area_name)
+VALUES ('ID');
+INSERT INTO AREAS (area_name)
+VALUES ('CO');
+INSERT INTO AREAS (area_name)
+VALUES ('AZ');
+INSERT INTO AREAS (area_name)
+VALUES ('Connecticut');
+INSERT INTO AREAS (area_name)
+VALUES ('MA');
+INSERT INTO AREAS (area_name)
+VALUES ('MS');
+INSERT INTO AREAS (area_name)
+VALUES ('FL');
+INSERT INTO AREAS (area_name)
+VALUES ('Michigan');
+INSERT INTO AREAS (area_name)
+VALUES ('OK');
+INSERT INTO AREAS (area_name)
+VALUES ('CO');
+INSERT INTO AREAS (area_name)
+VALUES ('California');
+INSERT INTO AREAS (area_name)
+VALUES ('MN');
+INSERT INTO AREAS (area_name)
+VALUES ('Kentucky');
+INSERT INTO AREAS (area_name)
+VALUES ('WY');
+INSERT INTO AREAS (area_name)
+VALUES ('Minnesota');
+INSERT INTO AREAS (area_name)
+VALUES ('IL');
+INSERT INTO AREAS (area_name)
+VALUES ('MT');
+INSERT INTO AREAS (area_name)
+VALUES ('TN');
+INSERT INTO AREAS (area_name)
+VALUES ('Wyoming');
+INSERT INTO AREAS (area_name)
+VALUES ('OR');
+INSERT INTO AREAS (area_name)
+VALUES ('MO');
+INSERT INTO AREAS (area_name)
+VALUES ('Florida');
+INSERT INTO AREAS (area_name)
+VALUES ('Alaska');
